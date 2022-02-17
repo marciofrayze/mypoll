473c74ef03d494ec392863f5cafb536095c43b6e
Marcio Frayze <mfdavid@gmail.com>
2022-02-17 15:25:13 -0300

    fix: typo

--- mypoll/asgi.py
+++ mypoll/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mypool project.
+ASGI config for mypoll project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypoll.settings')
 
 application = get_asgi_application()
--- mypoll/wsgi.py
+++ mypoll/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mypool project.
+WSGI config for mypoll project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypoll.settings')
 
 application = get_wsgi_application()
