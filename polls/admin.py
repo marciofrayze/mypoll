42c25981d2533ffbb1579b8a59a4f5e0687516fc
Marcio Frayze <mfdavid@gmail.com>
2022-02-17 09:40:11 -0300

    impl: ordering fields

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Publication Date comes first
+    """
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
