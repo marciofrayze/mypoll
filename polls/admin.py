eee8e6380d475f0f30b47fcfaa9e17c8012a0eae
Marcio Frayze <mfdavid@gmail.com>
2022-02-17 09:50:49 -0300

    impl: add default Choices for Question

--- polls/admin.py
+++ polls/admin.py
@@ -3,15 +3,20 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     """
-    Giving each fields a title and ordering it.
+    Giving each fields a title and ordering it. Also, add the Choices by default.
     """
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
